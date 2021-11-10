c10908eca230c2ccc3321824b72444b2ca0b15ca
alfan mudhofir <alfan.nutech@gmail.com>
2021-11-10 14:30:20 +0700

    End java generic

---
+++ belajar-java-generic/src/belajar/java/generic/application/ComparatorApp.java
@@ -0,0 +1,27 @@
+package belajar.java.generic.application;
+
+import belajar.java.generic.Person;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ComparatorApp {
+    public static void main(String[] args) {
+        Person[] people = {
+                new Person("Alimin", "Depok"),
+                new Person("Alfan", "Purwakarta"),
+                new Person("Ari", "Cikampek")
+        };
+
+        Comparator<Person> comparator = new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+
+        Arrays.sort(people, comparator);
+
+        System.out.println(Arrays.toString(people));
+    }
+}
